be073ec53309873a3b0b47b8ebcde311c492c66a
Cyanoptila <februarys22@gmail.com>
2023-03-28 23:30:10 +0900

    ADD　メニュー項目用のconfigを追加

---
+++ src/features/configs/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "SmartHR",
+  description: "課題アプリ作り",
+  mainNav: [
+    { title: "SmartHRとは", href: "/" },
+    { title: "機能", href: "/" },
+    { title: "料金", href: "/" },
+    { title: "導入事例", href: "/" },
+    { title: "お知らせ", href: "/" },
+    { title: "イベント", href: "/" },
+    { title: "ログイン", href: "/" },
+  ],
+};
