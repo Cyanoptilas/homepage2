d54c9917e6ae60b40ece723bcaa0acd6cdd51315
Cyanoptilas <75958367+Cyanoptilas@users.noreply.github.com>
2023-04-04 13:05:50 +0900

    MOD リロード時にアイコンが大きくならないよう設定を追加

--- src/features/configs/site.ts
+++ src/features/configs/site.ts
@@ -125,3 +125,9 @@ export const siteConfig: siteConfig = {
     { title: "ログイン", href: "/" },
   ],
 };
+
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
