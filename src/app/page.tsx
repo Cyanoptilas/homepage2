12481d6ba81a715598edb9c2b27ec4599f1e9957
Cyanoptilas <75958367+Cyanoptilas@users.noreply.github.com>
2023-04-04 13:06:29 +0900

    MOD リロード時にアイコンが大きくならないよう設定を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,14 @@ import {
   Top,
 } from "../features/index";
 
+import { siteConfig } from "@/features/configs/site";
+
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false;
+
 export default function Home() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
