a970327373ea9dfab92b3d2adb5682d41eeb65ea
Cyanoptila <februarys22@gmail.com>
2023-05-09 17:27:07 +0900

    MOD デザイン微修正

--- src/features/landing/BarNo1.tsx
+++ src/features/landing/BarNo1.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
 
 export default function BarNo1() {
   return (
@@ -7,45 +7,66 @@ export default function BarNo1() {
         <VStack align="center">
           <HStack>
             <Text
-              fontSize="lg"
-              fontWeight="bold"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              fontSize="18px"
+              fontWeight="600"
+              color="#4c4c4c"
+              lineHeight="1.6"
               _before={{
-                content: '""',
-                width: "0px",
+                content: "''",
+                display: "inline-block",
+                height: "28px",
+                width: "0",
+                marginLeft:"5px",
                 borderRight: "2px",
                 borderStyle: "solid",
                 borderColor: "#d2d2d2",
-                transform: `translateX(-22%) translateY(0) rotate(-28deg)`,
+                transform: "translateX(-22px) translateY(0) rotate(-28deg)",
               }}
               _after={{
                 content: '""',
-                width: "0px",
+                display: "inline-block",
+                height: "28px",
+                width: "0",
                 borderLeft: "2px",
                 borderStyle: "solid",
                 borderColor: "#d2d2d2",
-                transform: `translateX(22%) translateY(0) rotate(28deg)`,
+                transform: "translateX(10px) translateY(0) rotate(28deg)",
               }}
             >
-              登録社数50,000社以上。※1様々な業界の企業にご利用いただいています。
+              登録社数50,000社以上。
+              <Box
+                fontSize="10px"
+                display="inline-block"
+                margin="0 8px 0 -8px"
+                paddingBottom="6px"
+                position="relative"
+                verticalAlign="baseline"
+              >
+                ※1
+              </Box>
+              様々な業界の企業にご利用いただいています。
             </Text>
           </HStack>
-          <HStack spacing="8">
+          <HStack spacing="8" paddingTop="7px">
             <Image
               src="/image/character/banner-share-no1.png"
               alt="労務管理クラウド"
-              width={"33%"}
+              width={"30%"}
               objectFit="contain"
             ></Image>
             <Image
               src="/image/character/banner-satisfaction-no1.png"
               alt="人事労務システム"
-              width={"33%"}
+              width={"30%"}
               objectFit="contain"
             ></Image>
             <Image
               src="/image/character/banner-talent-no1.png"
               alt="タレントマネジメントシステム"
-              width={"33%"}
+              width={"30%"}
               objectFit="contain"
             ></Image>
           </HStack>
