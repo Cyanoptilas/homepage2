ea6650398b2712e5f0f7acf247af8d3d9201f96d
Cyanoptila <februarys22@gmail.com>
2023-05-10 17:13:27 +0900

    MOD デザイン微修正

--- src/features/landing/BarNo1.tsx
+++ src/features/landing/BarNo1.tsx
@@ -3,9 +3,9 @@ import { Box, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
 export default function BarNo1() {
   return (
     <>
-      <Box width={920}>
+      <Box>
         <VStack align="center">
-          <HStack>
+          <Flex>
             <Text
               display="flex"
               justifyContent="center"
@@ -25,6 +25,29 @@ export default function BarNo1() {
                 borderColor: "#d2d2d2",
                 transform: "translateX(-22px) translateY(0) rotate(-28deg)",
               }}
+            >
+              登録社数50,000社以上。
+            </Text>
+            <Text
+              fontSize="10px"
+              fontWeight="600"
+              // marginLeft="-10px"
+              margin="0 8px 0 -8px"
+              paddingBottom="-10px"
+              position="relative"
+              verticalAlign="baseline"
+              lineHeight="1.6"
+            >
+              ※1
+            </Text>
+            <Text
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              fontSize="18px"
+              fontWeight="600"
+              color="#4c4c4c"
+              lineHeight="1.6"
               _after={{
                 content: '""',
                 display: "inline-block",
@@ -36,37 +59,26 @@ export default function BarNo1() {
                 transform: "translateX(10px) translateY(0) rotate(28deg)",
               }}
             >
-              登録社数50,000社以上。
-              <Box
-                fontSize="10px"
-                display="inline-block"
-                margin="0 8px 0 -8px"
-                paddingBottom="6px"
-                position="relative"
-                verticalAlign="baseline"
-              >
-                ※1
-              </Box>
               様々な業界の企業にご利用いただいています。
             </Text>
-          </HStack>
+          </Flex>
           <HStack spacing="8" paddingTop="7px">
             <Image
               src="/image/character/banner-share-no1.png"
               alt="労務管理クラウド"
-              width={"30%"}
+              width="280px"
               objectFit="contain"
             ></Image>
             <Image
               src="/image/character/banner-satisfaction-no1.png"
               alt="人事労務システム"
-              width={"30%"}
+              width="280px"
               objectFit="contain"
             ></Image>
             <Image
               src="/image/character/banner-talent-no1.png"
               alt="タレントマネジメントシステム"
-              width={"30%"}
+              width="280px"
               objectFit="contain"
             ></Image>
           </HStack>
