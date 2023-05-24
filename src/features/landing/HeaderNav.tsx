d0480ad7b3bafec3d8952d7904725c18f49149d8
Cyanoptila <februarys22@gmail.com>
2023-05-24 17:10:46 -0400

    MOD HeaderNavを統一

--- src/features/landing/HeaderNav.tsx
+++ src/features/landing/HeaderNav.tsx
@@ -1,8 +1,22 @@
-import { Flex } from "@chakra-ui/react";
-import HeaderNavLeft from "./HeaderNavLeft";
-import HeaderNavRight from "./HeaderNavRight";
+import { Box, Flex, List, ListItem, Text } from "@chakra-ui/react";
+
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { siteConfig } from "@/features/configs/site";
+import { useHoveredMenuItem } from "./HeaderContainer";
+import { useWindowScroll } from "react-use";
+import {
+  ButtonContact,
+  ButtonEvent,
+  ButtonLogin,
+  ButtonUsefulDocs,
+} from "../ui/index";
 
 export default function HeaderNav() {
+  const { x, y } = useWindowScroll();
+  const [value, setValue] = useHoveredMenuItem();
+
   return (
     <Flex
       flexGrow="1"
@@ -10,8 +24,54 @@ export default function HeaderNav() {
       alignItems="center"
       justifyContent="space-between"
     >
-      <HeaderNavLeft />
-      <HeaderNavRight />
+      <Flex alignItems="center">
+        <List listStyleType="none" display="flex">
+          {siteConfig.mainNav.map((item) => {
+            return (
+              <Flex key={item.title}>
+                <ListItem
+                  key={item.title}
+                  display="flex"
+                  position="relative"
+                  fontWeight="600"
+                  fontSize="14px"
+                  height="14px"
+                  lineHeight="1"
+                  color={y > 80 || value != "0" ? "#777" : "white"}
+                  transition="color .3s ease"
+                  marginRight="28px"
+                  onMouseOver={() => setValue(item.id!)}
+                  _before={{
+                    content: "''",
+                    position: "absolute",
+                    bottom: "-30px",
+                    width: "100%",
+                    zIndex: "2",
+                    height: "4px",
+                    backgroundColor: "#00c4cc",
+                    transition: "transform 0.3s ease-in-out",
+                    transform: item.id === value ? "scaleX(1)" : "scaleX(0)",
+                  }}
+                >
+                  {item.title}
+                  <Box marginLeft="8px">
+                    <FontAwesomeIcon icon={faAngleDown} size={"xs"} />
+                  </Box>
+                </ListItem>
+              </Flex>
+            );
+          })}
+        </List>
+        <ButtonEvent />
+      </Flex>
+      <Box>
+        <Flex>
+          <ButtonLogin />
+
+          <ButtonContact />
+          <ButtonUsefulDocs />
+        </Flex>
+      </Box>
     </Flex>
   );
 }
